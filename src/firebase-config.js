25c478859164173dab873fc5499678e62c4be049
jagadeshwaran <76894794+jagadeshwarandev@users.noreply.github.com>
2021-05-01 01:30:59 -0700

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,12 +1,7 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyD_mgIXMyOdXTSpDYe50isH1wTBsLJjXHs",
-  authDomain: "todoapp-18cf5.firebaseapp.com",
-  projectId: "todoapp-18cf5",
-  storageBucket: "todoapp-18cf5.appspot.com",
-  messagingSenderId: "871774965280",
-  appId: "1:871774965280:web:da2373e776707412347645"
+  //API key comes here
 });
 
 const db = firebaseApp.firestore();
